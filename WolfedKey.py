771622bec15deb3ad9bad0a9ed6405356e6e7b9a
StackWolfed <sesilaso@gmail.com>
2018-12-21 18:33:45 +0100

    added the other functions - not tested

--- WolfedKey.py
+++ WolfedKey.py
@@ -17,18 +17,33 @@ except:
 # while True:
 #     print(chr((ser.readline().decode('utf-8'))))
 
+
+def editButton():
+    btn = str(input("Button number: "))
+    buffer = []
+    for i in range(0, 9):
+        if i % 2 == 0:
+            buffer.append(str(input(i)))
+        else:
+            buffer.append(str(chr(int(float(input(i))))))
+    sendMsg = ""
+    sendMsg = str(sendMsg + btn + buffer[0] + buffer[1] + buffer[2] + buffer[3] + buffer[4] + buffer[5] + buffer[6] + buffer[7] + buffer[8])
+    print(sendMsg)
+    ser.write(bytes(sendMsg.encode('utf-8')))
+
+def readButton():
+    btn = str(input("Button number: "))
+    sendMsg = "`" + btn;
+    ser.write(sendMsg.encode('utf-8'))
+    for i in range(0, 9):
+        print(ser.readline().decode('utf-8'))
+
 while True:
-    menumsg = "Select Option:\n1 = Configure new button combo\n2 = Read Button\n"
+    menumsg = "Select Option:\n1 = Configure new button combo\n2 = Read Button\n3 = Exit"
     userin = input(menumsg)
     if userin == "1":
-        btn = str(input("Button number: "))
-        buffer = []
-        for i in range(0, 9):
-            if i % 2 == 0:
-                buffer.append(str(input(i)))
-            else:
-                buffer.append(str(chr(int(float(input(i))))))
-        sendMsg = ""
-        sendMsg = str(sendMsg + btn + buffer[0] + (buffer[1]) + buffer[2] + (buffer[3]) + buffer[4] + (buffer[5]) + buffer[6] + (buffer[7]) + buffer[8])
-        print(sendMsg)
-        ser.write(bytes(sendMsg.encode('utf-8')))
+        editButton()
+    elif userin == "2":
+        readButton()
+    else:
+        exit()
